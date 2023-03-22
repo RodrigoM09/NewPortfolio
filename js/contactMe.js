a6a7e9f4d39f047c30427f7253d04db9968f58f6
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-22 18:33:54 -0500

    autosave script

---
+++ js/contactMe.js
@@ -0,0 +1,13 @@
+// let message = "";
+//
+// $("#submit_contact").on("click", function() {
+//     message = $("#contactForm").serialize();
+//     $.ajax({
+//         url: "//formspree.io/rodrigo.nmn.marquez@gmail.com",
+//         method: "POST",
+//         data: {message: message},
+//         dataType: "json"
+//     });
+//     alert('Thanks for the email, we\'ll be in touch promptly.');
+//     return false;
+// });
