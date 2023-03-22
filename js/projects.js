beb321addc278c1ad91bbdfdd7563df367ca0e4e
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-21 20:43:56 -0500

    autosave script

--- js/projects.js
+++ js/projects.js
@@ -1,33 +1,3 @@
-// let path = document.querySelector('path');
-// let pathLength = path.getTotalLength();
-//
-// path.style.strokeDasharray = pathLength + ' ' + pathLength;
-//
-// path.style.strokeDashoffset = pathLength;
-//
-// window.addEventListener('scroll', function(e) {
-//     let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-//     let drawLength = pathLength * scrollPercentage;
-//
-//     path.style.strokeDashoffset = pathLength - drawLength;
-//
-//     const target = document.querySelector('.scroll');
-//
-//     let index = 0, length = target.length;
-//     for (index; index < length; index++) {
-//         let pos = window.pageYOffset * target[index].dataset.rate;
-//
-//         if (target[index].dataset.direction === 'vertical') {
-//             target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
-//         }else{
-//             let posX = window.pageYOffset * target[index].dataset.ratex;
-//             let posY = window.pageYOffset * target[index].dataset.ratey;
-//
-//             target[index].style.transform = 'translate3d(' + posX + 'px,' + posY + 'px, 0px)';
-//         }
-//     }
-// });
-
 //Show Dialog
 $(".clickForMoreInfo").click(function () {
     $(this).next(".dialog").show();
@@ -51,22 +21,3 @@ const background = document.getElementsByClassName('.information_container');
 projects.addEventListener('mouseover', (e) => {
     background.style.opacity = '1';
 });
-
-
-
-
-// const overlays = document.querySelectorAll('.overlay');
-//
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         // Toggle the show class on the overlays elements
-//         overlays.forEach(overlay => {
-//             overlay.classList.toggle('show', entry.isIntersecting);
-//         });
-//     });
-// }, {
-//     // rootMargin: '100% 0% 100% 0%'
-//     threshold: 1
-// });
-//
-// observer.observe(overlays);
