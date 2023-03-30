ed765a7fc2222f8c68c27e90f1a37d628ee44c6b
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-30 15:13:07 -0500

    autosave script

--- js/contactMe.js
+++ js/contactMe.js
@@ -12,15 +12,71 @@
 //     return false;
 // });
 
-function validate(){
-    let mail = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    if(regex.test(mail)){
-        alert("Thanks for the email, we'll be in touch promptly.");
-        return true;
-    } else {
-        alert("Please enter a valid email address.");
-        return false;
-    }
+//Create an empty object for the fields
+let fields = {};
+document.addEventListener("DOMContentLoaded", function (){
+    fields.name = document.getElementById('name');
+    fields.email = document.getElementById('email');
+    fields.company =  document.getElementById('company-name');
+    fields.phone = document.getElementById('phone-number');
+    fields.message = document.getElementById('subject');
+});
+
+function isNotEmpty(value){
+    if(value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
 }
+function isPhoneNumber(phone){
+    let regex = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return regex.test(phone);
+}
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+}
+function fieldValidation(field, validationFunction){
+    if(field == null) return false;
+    let isFieldValid = validationFunction(field.value)
+    if(!isFieldValid){
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+    return isFieldValid;
+}
+function isValid(){
+    let valid = true;
+    valid &= fieldValidation(fields.name, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.company, isNotEmpty);
+    valid &= fieldValidation(fields.phone, isPhoneNumber);
+    valid &= fieldValidation(fields.message, isNotEmpty);
+    return valid;
+}
+document.getElementById('contactForm').addEventListener('submit', function (event){
+    event.preventDefault();
+    alert('Thanks for the email, we\'ll be in touch promptly.');
+    if(isValid()){
+        // Prepare the data to be sent to formspree
+        let templateParams = {
+            name: fields.name.value,
+            email: fields.email.value,
+            company: fields.company.value,
+            phone: fields.phone.value,
+            message: fields.message.value
+        }
+        // Send the data to Formspree
+        emailjs.send("service_1fjxxpd", "template_zn6tdud", templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            // Clear the form
+            fields.name.value = '';
+            fields.email.value = '';
+            fields.company.value = '';
+            fields.phone.value = '';
+            fields.message.value = '';
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    }
+});
