d0bb6eb2c817227949e589f68dd2f41ee65426cc
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-30 12:09:55 -0500

    autosave script

--- js/contactMe.js
+++ js/contactMe.js
@@ -11,3 +11,16 @@
 //     alert('Thanks for the email, we\'ll be in touch promptly.');
 //     return false;
 // });
+
+function validate(){
+    let mail = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    if(regex.test(mail)){
+        alert("Thanks for the email, we'll be in touch promptly.");
+        return true;
+    } else {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+}
